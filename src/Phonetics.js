47a6a671835a99b3ae606f0c62900cb3c1c0041c
Barbara Jandernoa <bajandernoa@gmail.com>
2021-04-23 18:27:46 -0400

    add rel=noreferrer to avoid security risk on link that opens new window

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Phonetics(props) {
     return (
         <div className="Phonetics">
-           <a href={props.phonetics.audio} target="_blank">
+           <a href={props.phonetics.audio} target="_blank" rel="noreferrer">
                 Listen
            </a>
            <br /> 
